0c3da8222ae7025a99148659ac08a5dbb21c0d00
kerrieyee <kerrie@kerrieyee.com>
2012-12-01 14:09:22 -0800

    Update spec/controllers/gists_controller_spec.rb

--- spec/controllers/gists_controller_spec.rb
+++ spec/controllers/gists_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 describe GistsController do 
 	describe "#index" do
 		it "assigns instance variables" do
-			get :index, :username => "bleh"
+			get :index, :username => "kerrieyee"
 			assigns(:username).should_not be_nil
 			assigns(:gists).should_not be_nil
 		end
