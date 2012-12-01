c7ac3068c26828a72cb46eb3be2ba5bdccef7900
kerrieyee <kerrie@kerrieyee.com>
2012-12-01 14:07:12 -0800

    Update app/controllers/Gists_controller.rb
    Removed unnecessary comments

--- app/controllers/Gists_controller.rb
+++ app/controllers/Gists_controller.rb
@@ -5,18 +5,6 @@ class GistsController < ApplicationController
 	@gists = github.gists.list user: @username
 	end
 
-	# def create
-	# 	@username = params[:username]
-	# 	github = Github.new
-	# 	@gists = github.gists.list user: params[:username]
-	# 	redirect_to browsing_path(create)
-	# end
-
-	# def show
-	# 	github = Github.new
-	# 	@gists = github.gists.list user: params[:username]
-	# 	# @title = params[:username] + "'s gists"
-	# end
 end
 
 
